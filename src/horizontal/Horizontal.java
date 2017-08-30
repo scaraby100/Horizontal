03ce49bdfbd0effa0cf0a79cebedce2efa38a479
BuildTools <unconfigured@null.spigotmc.org>
2017-08-30 09:01:59 +0200

    Add parameter

--- src/horizontal/Horizontal.java
+++ src/horizontal/Horizontal.java
@@ -6,10 +6,12 @@
 package horizontal;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -25,13 +27,18 @@ public class Horizontal
     public Horizontal()
     {
         this.returnId = 0;
-        value = new HashMap<>();
-        isReady = new HashMap<>();
+        value = new ConcurrentHashMap<>();
+        isReady = new ConcurrentHashMap<>();
     }
 
     public void run(Object job, String methodName)
     {
         threadStart(job, methodName, false, 0);
+    }
+    
+    public void run(Object job, String methodName, Object args)
+    {
+        threadStart(job, methodName, false, 0, args);
     }
 
     public long runWithReturn(Object job, String methodName)
@@ -42,11 +49,26 @@ public class Horizontal
         threadStart(job, methodName, true, returnIndex);
         return returnIndex;
     }
+    
+    public long runWithReturn(Object job, String methodName, Object args)
+    {
+        long returnIndex = returnId;
+        returnId++;
+        isReady.put(returnIndex, false);
+        threadStart(job, methodName, true, returnIndex, args);
+        return returnIndex;
+    }
 
     public void runWithThread(Object job, String methodName, int thread)
     {
         for (int i = 0; i < thread; i++)
             run(job, methodName);
+    }
+    
+    public void runWithThread(Object job, String methodName, int thread, Object args)
+    {
+        for (int i = 0; i < thread; i++)
+            run(job, methodName, args);
     }
 
     public Set<Long> runWithThreadAndReturn(Object job, String methodName, int thread)
@@ -56,15 +78,33 @@ public class Horizontal
             returnIds.add(runWithReturn(job, methodName));
         return returnIds;
     }
+    
+    public Set<Long> runWithThreadAndReturn(Object job, String methodName, int thread, Object args)
+    {
+        Set<Long> returnIds = new HashSet<>();
+        for (int i = 0; i < thread; i++)
+            returnIds.add(runWithReturn(job, methodName, args));
+        return returnIds;
+    }
 
     public void runOnMaxCores(Object job, String methodName)
     {
         runWithThread(job, methodName, Runtime.getRuntime().availableProcessors());
     }
+    
+    public void runOnMaxCores(Object job, String methodName, Object args)
+    {
+        runWithThread(job, methodName, Runtime.getRuntime().availableProcessors(), args);
+    }
 
     public Set<Long> runOnMaxCoresWithReturn(Object job, String methodName)
     {
         return runWithThreadAndReturn(job, methodName, Runtime.getRuntime().availableProcessors());
+    }
+    
+    public Set<Long> runOnMaxCoresWithReturn(Object job, String methodName, Object args)
+    {
+        return runWithThreadAndReturn(job, methodName, Runtime.getRuntime().availableProcessors(), args);
     }
 
     public Object getValue(long index)
@@ -95,7 +135,33 @@ public class Horizontal
                 }
                 catch (IllegalAccessException | IllegalArgumentException | NoSuchMethodException | SecurityException | InvocationTargetException e)
                 {
-                    //TODO: ADD EXCEPTION MANAGEMENT 
+                    Logger.getLogger(Horizontal.class.getName()).log(Level.SEVERE, null, e); 
+                }
+
+            }
+        };
+        thread.start();
+    }
+    
+    private void threadStart(Object job, String methodName, boolean returnMode, long returnIndex, Object args)
+    {
+        Thread thread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    if (returnMode)
+                    {
+                        value.put(returnIndex, job.getClass().getDeclaredMethod(methodName, args.getClass()).invoke(job, args));
+                        isReady.put(returnIndex, true);
+                    } else
+                        job.getClass().getDeclaredMethod(methodName, args.getClass()).invoke(job, args);
+                }
+                catch (IllegalAccessException | IllegalArgumentException | NoSuchMethodException | SecurityException | InvocationTargetException e)
+                {
+                    Logger.getLogger(Horizontal.class.getName()).log(Level.SEVERE, null, e);
                 }
 
             }
